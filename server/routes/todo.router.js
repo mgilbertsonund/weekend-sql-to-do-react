bb629283701a542dabb0af80fcef35e6671e0302
Matt Gilbertson <mgilbertsonund@gmail.com>
2024-03-23 10:48:59 -0500

    created router GET request

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,7 +3,18 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
-
+router.get('/', (req, res) => {
+    console.log('In GET request');
+    let queryText = 'SELECT * from "tasks";';
+    pool.query(queryText)
+        .then((result) => {
+            res.send(result.rows);
+        })
+        .error((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        })
+});
 // POST
 
 // PUT
