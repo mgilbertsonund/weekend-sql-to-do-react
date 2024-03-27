d03de72bb1709f444650b7a6f49a01e5c399dcdb
Matt Gilbertson <mgilbertsonund@gmail.com>
2024-03-27 09:08:47 -0500

    added my inpit and array variable, and created an input box

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,23 @@
 import {useState} from 'react';
 
 function App () {
+  // create my array of tasks and input variable
+  const [todoList, setToDoList] = useState([]);
+  const [inputTask, setInputTask] = useState('');
+
+
   
   return (
     <div>
       <h1>TO DO APP</h1>
+      <input
+        value = {inputTask} 
+        type = "text" 
+        onChange={e => setInputTask(e.target.value)}
+      />
+      <button>
+        Add
+      </button>
     </div>
   );
 
